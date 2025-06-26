5d514b87a360cf9c7f5971b58e4e640404663e84
j-ojey <georgewainaina409@gmail.com>
2025-06-27 01:45:21 +0300

    Finished Blackjack

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,20 +1,37 @@
-let firstCard = 10;
-let secondCard = 2;
-let sum = firstCard + secondCard;
-let cards = [firstCard, secondCard]
+let sum = 0;
+let cards = [];
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let cardsEl = document.getElementById("cards-el");
 let sumEl = document.getElementById("sum-el");
 
+let player = {
+  name: "George: ",
+  chips: 1000,
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + " $" + player.chips;
+
 function startGame() {
+  isAlive = true;
+  player.chips -= 50; 
+  playerEl.textContent = player.name + " $" + player.chips;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  sum = firstCard + secondCard;
+  cards = [firstCard, secondCard];
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -25,12 +42,25 @@ function renderGame() {
     message = "You're out of the game!";
     isAlive = false;
   }
-  messageEl.textContent = message
+  messageEl.textContent = message;
 }
 
-function newCard(){
-    let thirdCard = Math.floor(Math.random()*11)
-    cards.push(thirdCard)
-    sum += thirdCard
-    renderGame()
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let thirdCard = getRandomCard();
+    cards.push(thirdCard);
+    sum += thirdCard;
+    renderGame();
+  }
+}
+
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13 + 1);
+  if (randomCard > 10) {
+    return 10;
+  } else if (randomCard === 1) {
+    return 11;
+  } else {
+    return randomCard;
+  }
 }
